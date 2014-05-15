023a4eacbac119d6f2f39d305a1015be1127d602
Joey Ranieri <joey.ranieri@gmail.com>
2014-05-15 14:11:59 -0400

    Initial Commit-Books class
    Create all the functions into a Books class

--- asoiaf-reddit.py
+++ asoiaf-reddit.py
@@ -17,26 +17,6 @@ import time
 # GLOBALS
 # =============================================================================
 
-# Reads the config file
-config = ConfigParser.ConfigParser()
-config.read("asoiafsearchbot.cfg")
-
-user_agent = ("ASOIAFSearchBot -Help you find that comment- by /u/RemindMeBotWrangler")
-reddit = praw.Reddit(user_agent=user_agent)
-
-# Reddit Info
-reddit_user = config.get("Reddit", "username")
-reddit_pass = config.get("Reddit", "password")
-reddit.login(reddit_user, reddit_pass)
-
-# Database info
-host = config.get("SQL", "host")
-user = config.get("SQL", "user")
-passwd = config.get("SQL", "passwd")
-db = config.get("SQL", "db")
-table = config.get("SQL", "table")
-column1 = config.get("SQL", "column1")
-column2 = config.get("SQL", "column2")
 
 # commented already messaged are appended to avoid messaging again
 commented = []
@@ -52,6 +32,7 @@ ACOK = 'ACOK'
 ASOS = 'ASOS'
 AFFC = 'AFFC'
 ADWD = 'ADWD'
+NONE = 'NONE'
 
 # =============================================================================
 # CLASSES
@@ -83,271 +64,53 @@ class Connect(object):
     def close(self):
         self.connection.close()
 
-# =============================================================================
-# PUBLIC FUNCTIONS
-# =============================================================================
+class Books(object):
 
-def parse_comment(comment, book):
-    """
-    Parses comment for what term to search for
-    Also decides if it's sensitive or insensitive
-    """
-    if comment.id not in commented:
-        search_term = ""
-        sensitive = False
-        # remove everything before SearchCommand!
-        # Allows quotations to be used before SearchCommand!
-        original_comment = comment.body
+    _comments=None
+    _comment=None
+    _book=None
+    _searchTerm=None
+    _sensitive=False
 
-        if (book == ALL):
-            original_comment = ''.join(original_comment.split('SearchAll!')[1:])
-        elif (book == AGOT):
-            original_comment = ''.join(original_comment.split('SearchAGOT!')[1:])
-        elif (book == ACOK):
-            original_comment = ''.join(original_comment.split('SearchACOK!')[1:])
-        elif (book == ASOS):
-            original_comment = ''.join(original_comment.split('SearchASOS!')[1:])
-        elif (book == AFFC):
-            original_comment = ''.join(original_comment.split('SearchAFFC!')[1:])
-        elif (book == ADWD):
-            original_comment = ''.join(original_comment.split('SearchADWD!')[1:])
-
-        # Only reply to threads with Spoiler All
-        # TODO: Expand to each book spoiler tag scope
-        if re.match("(\(|\[).*(published|(spoiler.*all)|(all.*spoiler)).*(\)|\])", comment.link_title.lower()):
+    def __init__(self, comments):
+        pass
+    def parse_comment(self):
+        pass
+        if _comment.id not in commented and book != NONE:
+            _searchTerm=''.join(re.split(
+                                    r'Search(All|AGOT|ACOK|ASOS|AFFC|ADWD)!', 
+                                    _searchTerm)[2:]
+                                )
             # INSENSITIVE
-            search_brackets = re.search('"(.*?)"', original_comment)
+            search_brackets=re.search('"(.*?)"', _searchTerm)
             if search_brackets:
-                search_term = search_brackets.group(0)
-                sensitive = False
+                _searchTerm=search_brackets.group(0)
+                _sensitive=False
+            
 
             # SENSITIVE
-            search_tri = re.search('\((.*?)\)', original_comment)
+            search_tri=re.search('\((.*?)\)', _searchTerm)
             if search_tri:
-                search_term = search_tri.group(0)
-                sensitive = True
+                _searchTerm=search_tri.group(0)
+                _sensitive=True
 
-            # Stop pesky searches like "a"
-            if len(search_term) > 3:
-                search_db(comment, search_term, sensitive, book)
-
-        else:
-            message = (
-                "######&#009;\n\n####&#009;\n\n#####&#009;\n\n"
-                ">**Sorry, fulfilling this request would be a spoiler.**\n\n"
-                "_____\n"
-                "^(I'm ASOIAFSearchBot, I will display the occurrence of your "
-                "search term throughout the books. Only currently working in Spoiler All topics.) "
-                "[^(More Info Here)]"
-                "(http://www.reddit.com/r/asoiaf/comments/25amke/"
-                "spoilers_all_introducing_asoiafsearchbot_command/)\n\n"
-            )
-            reply(comment, message)
- 
-
-def search_db(comment, term, sensitive, book):
-    """
-    Queries through DB counts occurrences for each chapter
-    """
-    search_database = Connect()
-
-    # Take away whitespace and quotations at start and end
-    term = term[1:-1]
-    term = term.strip()
-
-    total = 0  # Total Occurrence
-    row_count = 0  # How many rows have been searched through
-
-    if not sensitive:
-
-        # INSENSITIVE SEARCH
-        if book == ALL:
-            search_database.execute(
-                'SELECT * FROM {table} WHERE lower({col1}) REGEXP '
-                '"([[:blank:][:punct:]]|^){term}([[:punct:][:blank:]]|$)" '
-                'ORDER BY FIELD'
-                '({col2}, "AGOT", "ACOK", "ASOS", "AFFC", "ADWD"), 2'.format(
-                    table=table,
-                    col1=column1,
-                    term=term,
-                    col2=column2
-                )
-            )
-
-       # Searches through individual books
-        else:
-            search_database.execute(
-            'SELECT * FROM {table} WHERE lower({col1}) REGEXP '
-            '"([[:blank:][:punct:]]|^){term}([[:punct:][:blank:]]|$)" '
-            'AND {col2} = "{book}" '
-            'ORDER BY FIELD'
-            '({col2}, "AGOT", "ACOK", "ASOS", "AFFC", "ADWD"), 2'.format(
-                    table=table,
-                    col1=column1,
-                    term=term,
-                    col2=column2,
-                    book=book
-                )
-            )
-
-        data = search_database.fetchall()
-        list_occurrence = []
-
-        # Counts occurrences in each row and
-        # adds itself to listOccurrence for message
-        for row in data:
-            list_occurrence.append(
-                "| {series}| {book}| {number}| {chapter}| {pov}| {occur}".format(
-                    series=row[0],
-                    book=row[1],
-                    number=row[2],
-                    chapter=row[3],
-                    pov=row[4],
-                    occur=len(
-                        re.findall("(\W|^)" + term.lower() +
-                                   "(\W|$)", row[5].lower())
-                    )
-                )
-            )
-            total += len(
-                re.findall("(\W|^)" + term.lower() +
-                           "(\W|$)", row[5].lower())
-            )
-            row_count += 1
-
-    else:
-
-        # SENSITIVE SEARCH
-        if book == ALL:
-
-            search_database.execute(
-                'SELECT * FROM {table} WHERE {col1} REGEXP BINARY '
-                '"([[:blank:][:punct:]]|^){term}([[:punct:][:blank:]]|$)" '
-                'ORDER BY FIELD'
-                '({col2}, "AGOT", "ACOK", "ASOS", "AFFC", "ADWD"), 2'.format(
-                    table=table,
-                    col1=column1,
-                    term=term,
-                    col2=column2
-                )
-            )
-
-        # Searches through individual books
-        else:
-            search_database.execute(
-                'SELECT * FROM {table} WHERE {col1} REGEXP BINARY '
-                '"([[:blank:][:punct:]]|^){term}([[:punct:][:blank:]]|$)" '
-                'AND {col2} = "{book}" '
-                'ORDER BY FIELD'
-                '({col2}, "AGOT", "ACOK", "ASOS", "AFFC", "ADWD"), 2'.format(
-                    table=table,
-                    col1=column1,
-                    term=term,
-                    col2=column2,
-                    book=book
-                )
-            )
-
-        data = search_database.fetchall()
-        list_occurrence = []
-        # Counts occurrences in each row and
-        # adds itself to listOccurrence for message
-        for row in data:
-            list_occurrence.append(
-                "| {series}| {book}| {number}| {chapter}| {pov}| {occur}".format(
-                    series=row[0],
-                    book=row[1],
-                    number=row[2],
-                    chapter=row[3],
-                    pov=row[4],
-                    occur=len(
-                        re.findall("(\W|^)" + term +
-                                   "(\W|$)", row[5])
-                    )
-                )
-            )
-            total += len(
-                re.findall("(\W|^)" + term +
-                           "(\W|$)", row[5])
-            )
-            row_count += 1
-
-    search_database.close()
-    build_message(comment, list_occurrence, row_count, total, term, sensitive)
+    def search_db():
+        pass
+        searchDb=Connect()
 
 
-
-def build_message(comment, occurrence, row_count, total, term, sensitive):
-    """
-    Sends message to user with the requested information
-    """
-
-    message = ""
-    comment_to_user = (
-        "######&#009;\n\n####&#009;\n\n#####&#009;\n\n"
-        "**SEARCH TERM ({caps}): {term}** \n\n "
-        "Total Occurrence: {occur} \n\n"
-        ">{message}"
-        "\n[Visualization of the search term]"
-        "(http://creative-co.de/labs/songicefire/?terms={term})"
-        "\n_____\n "
-        "^(I'm ASOIAFSearchBot, I will display the occurrence of your "
-        "search term throughout the books. Only currently working in Spoiler All topics.) "
-        "[^(More Info Here)]"
-        "(http://www.reddit.com/r/asoiaf/comments/25amke/"
-        "spoilers_all_introducing_asoiafsearchbot_command/)"
-    )
-
-    # Avoid spam, limit amount of rows
-    if row_count < 31 and total > 0:
-        message += (
-            "| Series| Book| Chapter| Chapter Name| Chapter POV| Occurrence\n"
-        )
-        message += "|:{dash}|:{dash}|:{dash}|:{dash}|:{dash}|:{dash}|\n".format(
-            dash='-' * 11
-        )
-        # Each element is changed to one string
-        for row in occurrence:
-            message += row + "\n"
-    elif row_count > 31:
-        message = "**Excess number of chapters.**\n\n"
-    elif total == 0:
-        message = "**Sorry no results.**\n\n"
-
-    case_sensitive = "CASE-SENSITIVE" if sensitive else "CASE-INSENSITIVE"
-
-    comment_to_user = comment_to_user.format(
-        caps=case_sensitive,
-        term=term,
-        occur=total,
-        message=message
-    )
-
-    if sensitive:
-        term_history_sensitive[term] = comment_to_user
-    else:
-        term_history[term.lower()] = comment_to_user
-
-    reply(comment, comment_to_user)
-
-
-
-
-def reply(comment, full_reply):
-    """Replies to a comment with the text provided"""
-    try:
-        comment.reply(full_reply)
-        print full_reply
-    except (HTTPError, ConnectionError, Timeout, timeout) as err:
-        print err
-    except RateLimitExceeded as err:
-        print err
-        time.sleep(10)
-    except APIException as err:  # Catch any less specific API errors
-        print err
-    else:
-        commented.append(comment.id)
-        
+    def spoiler_book(self):
+        #TODO: add the other regular expressions 
+        if re.match(
+            "(\(|\[).*(published|(spoiler.*all)|(all.*spoiler)).*(\)|\])", 
+            _comment.link_title.lower()
+        ):
+            _book=ALL
+        if re.match(
+            "REGEX HERE",
+            _comment.link_title.lower()
+        ):
+            _book=AGOT
 
 # =============================================================================
 # MAIN
@@ -356,38 +119,36 @@ def reply(comment, full_reply):
 
 def main():
     """Main runner"""
+    try:
+        # Reddit Info
+        user_agent = (
+                "ASOIAFSearchBot -Help you find that comment"
+                "- by /u/RemindMeBotWrangler")
+        reddit=praw.Reddit(user_agent=user_agent)
+        reddit_user=config.get("Reddit", "username")
+        reddit_pass=config.get("Reddit", "password")
+        reddit.login(reddit_user, reddit_pass)
+
+    except Exception as err:
+        print err
+
     while True:
         try:
-            print "start"
-            # Grab all new comments from /r/asoiaf
-            comments = praw.helpers.comment_stream(
+            commentCount=0
+            allBooks=Books()
+            for comment in praw.helpers.comment_stream(
                 reddit, 'asoiaf', limit=None, verbosity=0
-            )
-            comment_count = 0
-            # Loop through each comment
-            for comment in comments:
+            ):
                 comment_count += 1
-                
-                if "SearchAll!" in comment.body:
-                    parse_comment(comment, ALL)
-                elif "SearchAGOT!" in comment.body:
-                    parse_comment(comment, AGOT)
-                elif "SearchACOK!" in comment.body:
-                    parse_comment(comment, ACOK)
-                elif "SearchASOS!" in comment.body:
-                    parse_comment(comment, ASOS)
-                elif "SearchAFFC!" in comment.body:
-                    parse_comment(comment, AFFC)
-                elif "SearchADWD!" in comment.body:
-                    parse_comment(comment, ADWD)
+                spoiler_book()
+                parse_comment()
+                # Stops pesky searches like "a"
+                if len(allBook._searchTerm) > 3:
+                    search_db
 
-                if comment_count == 1000:
-                    break
-            print "sleeping"
-            time.sleep(25)
+
         except Exception as err:
             print err
-
 # =============================================================================
 # RUNNER
 # =============================================================================
