94fb94b65788c33476888d88f12cc914659d10ff
Joey Ranieri <joey.ranieri@gmail.com>
2014-05-15 18:57:26 -0400

    Worked on search_db() func

--- asoiaf-reddit.py
+++ asoiaf-reddit.py
@@ -17,6 +17,17 @@ import time
 # GLOBALS
 # =============================================================================
 
+config = ConfigParser.ConfigParser()
+config.read("asoiafsearchbot.cfg")
+
+# Database info
+host = config.get("SQL", "host")
+user = config.get("SQL", "user")
+passwd = config.get("SQL", "passwd")
+db = config.get("SQL", "db")
+table = config.get("SQL", "table")
+column1 = config.get("SQL", "column1")
+column2 = config.get("SQL", "column2")
 
 # commented already messaged are appended to avoid messaging again
 commented = []
@@ -66,37 +77,66 @@ class Connect(object):
 
 class Books(object):
 
-    _comments=None
-    _comment=None
-    _book=None
-    _searchTerm=None
-    _sensitive=False
-
+    _comments = None
+    _comment = None
+    _book = None
+    _searchTerm = None
+    _sensitive = False
+    _total = None
+    _rowCount = None
+    _searchDb = Connect()
+    
+    
     def __init__(self, comments):
         pass
     def parse_comment(self):
         pass
         if _comment.id not in commented and book != NONE:
-            _searchTerm=''.join(re.split(
+            _searchTerm = ''.join(re.split(
                                     r'Search(All|AGOT|ACOK|ASOS|AFFC|ADWD)!', 
                                     _searchTerm)[2:]
                                 )
             # INSENSITIVE
-            search_brackets=re.search('"(.*?)"', _searchTerm)
+            search_brackets = re.search('"(.*?)"', _searchTerm)
             if search_brackets:
-                _searchTerm=search_brackets.group(0)
-                _sensitive=False
+                _searchTerm = search_brackets.group(0)
+                _sensitive = False
             
 
             # SENSITIVE
-            search_tri=re.search('\((.*?)\)', _searchTerm)
+            search_tri = re.search('\((.*?)\)', _searchTerm)
             if search_tri:
-                _searchTerm=search_tri.group(0)
-                _sensitive=True
+                _searchTerm = search_tri.group(0)
+                _sensitive = True
 
-    def search_db():
+    def search_db(self):
         pass
-        searchDb=Connect()
+        if sensitive:
+            mySqlSearch = 
+                'SELECT * FROM {table} WHERE lower({col1}) REGEXP '
+                '"([[:blank:][:punct:]]|^){term}([[:punct:][:blank:]]|$)" '
+                '({book})ORDER BY FIELD'
+                '({col2}, "AGOT", "ACOK", "ASOS", "AFFC", "ADWD"), 2'
+        else:
+            mySqlSearch =
+                'SELECT * FROM {table} WHERE {col1} REGEXP BINARY '
+                '"([[:blank:][:punct:]]|^){term}([[:punct:][:blank:]]|$)" '
+                '({book})ORDER BY FIELD'
+                '({col2}, "AGOT", "ACOK", "ASOS", "AFFC", "ADWD"), 2'
+        
+        # used for searching the book
+        bookSearch = ""
+        if _book != ALL:
+            bookSearch = _book
+
+        _searchDb.execute(mySqlSearch.format(
+                table = table,
+                col1 = column1
+                term = _searchTerm,
+                col2 = column2,
+                book = _book
+            )
+        )
 
 
     def spoiler_book(self):
@@ -105,12 +145,12 @@ class Books(object):
             "(\(|\[).*(published|(spoiler.*all)|(all.*spoiler)).*(\)|\])", 
             _comment.link_title.lower()
         ):
-            _book=ALL
+            _book = ALL
         if re.match(
             "REGEX HERE",
             _comment.link_title.lower()
         ):
-            _book=AGOT
+            _book = AGOT
 
 # =============================================================================
 # MAIN
@@ -124,9 +164,9 @@ def main():
         user_agent = (
                 "ASOIAFSearchBot -Help you find that comment"
                 "- by /u/RemindMeBotWrangler")
-        reddit=praw.Reddit(user_agent=user_agent)
-        reddit_user=config.get("Reddit", "username")
-        reddit_pass=config.get("Reddit", "password")
+        reddit = praw.Reddit(user_agent = user_agent)
+        reddit_user = config.get("Reddit", "username")
+        reddit_pass = config.get("Reddit", "password")
         reddit.login(reddit_user, reddit_pass)
 
     except Exception as err:
@@ -134,10 +174,10 @@ def main():
 
     while True:
         try:
-            commentCount=0
-            allBooks=Books()
+            commentCount = 0
+            allBooks = Books()
             for comment in praw.helpers.comment_stream(
-                reddit, 'asoiaf', limit=None, verbosity=0
+                reddit, 'asoiaf', limit = None, verbosity = 0
             ):
                 comment_count += 1
                 spoiler_book()
@@ -155,3 +195,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
